af6c03480c1033c946b0bb6fde27e5451a74b158
yoontto <jedney69@gmail.com>
2022-11-30 00:09:09 +0900

    백준 2단계 문제풀이 끝

--- src/baekjoon/Second_Conditional.java
+++ src/baekjoon/Second_Conditional.java
@@ -59,8 +59,94 @@ public class Second_Conditional {
         }
 
         System.out.println(result);*/
-        
 
+        //알람시계
+        //알람 45분 앞당기기
+        /*BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+
+        st = new StringTokenizer(br.readLine());
+
+        int h = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
+
+        int outM = m + 15;
+        if(outM >= 60){
+            m = outM - 60;
+        } else {
+            m = outM;
+            h -= 1;
+        }
+
+        if(h < 0){
+            h += 24;
+        }
+        System.out.print(h + " " +m);*/
+
+        //오븐 시계
+        /*BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+        st = new StringTokenizer(br.readLine());
+
+        int h = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
+        int cookTime = Integer.parseInt(br.readLine());
+
+        int min = m+cookTime;
+
+        if(min >= 60){
+            m = min % 60;
+            h += min / 60;
+        } else {
+            m += cookTime;
+        }
+
+        if(h >= 24){
+            h -=24;
+        }
+
+        System.out.print(h+" "+m);*/
+
+        //주사위 세개 
+        //와 이거 진짜 어렵다... 조건문에 왤케 약하지
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int a = Integer.parseInt(st.nextToken());
+        int b = Integer.parseInt(st.nextToken());
+        int c = Integer.parseInt(st.nextToken());
+
+        int prize = 0;
+
+        if(a == b && b == c){
+            prize = 10000 + a * 1000;
+        } else {
+            if(a != b && a != c && b != c){ // 세 수가 모두 다른 경우
+                int max;
+                if(a > b){
+                    if(c > a){ //c>a>b
+                        max = c;
+                    } else { // a>b,c
+                        max = a;
+                    }
+                } else { //b > a
+                    if(c > b){ //
+                        max = c;
+                    } else { // b>a, c<b
+                        max = b;
+                    }
+                }
+                prize = max * 100;
+            } else {
+                if(a == c || a == b){ //363
+                    prize = 1000 + a * 100;
+                } else { //633
+                    prize = 1000 + b * 100;
+                }
+            }
+
+        }
+        System.out.print(prize);
     }
 }
 
